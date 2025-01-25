b60c0a67c1a5d0792af311149265d141f1df484f
Jeffrey Bissel <jeffrey@bissel.it>
2025-01-25 16:30:53 +0100

    Give 'em Cake: Amunition (Kuchen-Animation)

--- game/level/give_them_cake/give_them_cake.gd
+++ game/level/give_them_cake/give_them_cake.gd
@@ -1,8 +1,8 @@
 extends level_base
 
 var cursor_cake = load("res://assets/give_them_cake/cursor.png")
+var cursor_empty = load("res://assets/give_them_cake/cursor_empty.png")
 const template_bird = preload("res://level/give_them_cake/lindner.tscn")
-# var template_point = preload("res://level/moorhuhn/point.tscn")
 
 var bpm = 20.0
 var rng = RandomNumberGenerator.new()
@@ -13,12 +13,16 @@ var max_points = 10
 var bird_holder: Node
 var points_holder: Node
 var points_visual: Control
+var amunition_visual: AnimatedSprite2D
 
 var _points_to_spawn = 0
 
-var _next_point_spanw_in_s = 0
-var _next_bird_spawn_in_s = 0.0
+var _next_reload_in_s: float = 0.0
+var _next_bird_spawn_in_s: float = 0.0
 
+var amunition = 0
+var reload = true
+const max_amunition = 4
 var _points = 200
 const point_multiplicator = 10
 const points_scale = 2.5
@@ -29,7 +33,9 @@ func _ready():
 	bird_holder = $"./BirdHolder"
 	points_holder = $"./PointsHolder"
 	points_visual = $"Control2/AspectRatioContainer/Control/PointsHolder/Points"
-	Input.set_custom_mouse_cursor(cursor_cake, 0, Vector2(128, 128))
+	amunition_visual = $"Control2/AspectRatioContainer/Control/Control2/Amunition"
+	
+	Input.set_custom_mouse_cursor(cursor_empty, 0, Vector2(128, 128))
 
 func _physics_process(delta):
 	_next_bird_spawn_in_s = _next_bird_spawn_in_s - delta
@@ -40,6 +46,16 @@ func _physics_process(delta):
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
 	points_visual.size.y = _points
+	amunition_visual.set_frame_and_progress(amunition, 0)
+	
+	if reload:
+		_next_reload_in_s -= _delta
+		if _next_reload_in_s < 0:
+			amunition += 1
+			_next_reload_in_s = 0.25
+			if amunition == max_amunition:
+				Input.set_custom_mouse_cursor(cursor_cake, 0, Vector2(128, 128))
+				reload = false
 	
 func _input(event):
 	if !event is InputEventMouseButton:
@@ -47,7 +63,15 @@ func _input(event):
 		
 	if event.button_index != MOUSE_BUTTON_LEFT or !event.pressed:
 		return
-			
+		
+	if reload:
+		return
+		
+	amunition -= 1
+	if amunition == 0:
+		Input.set_custom_mouse_cursor(cursor_empty, 0, Vector2(128, 128))
+		reload = true
+	
 	# $"AudioStreamClick".play()
 	var pos = get_viewport().get_camera_2d().get_local_mouse_position()
 	
