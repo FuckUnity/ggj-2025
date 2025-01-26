e12956603e8bb475b394805f866ecd1c0c2f04a9
Jeffrey Bissel <jeffrey@bissel.it>
2025-01-26 15:37:54 +0100

    Avoid Questions ende verzögert

--- game/level/avoid_questions/avoid_questions.gd
+++ game/level/avoid_questions/avoid_questions.gd
@@ -1,1 +1,12 @@
 extends level_base
+
+var _is_complete = false
+
+func _on_complete():
+	if _is_complete: 
+		return
+		
+	_is_complete = true
+	var tween = create_tween()
+	tween.tween_interval(2)
+	tween.tween_callback(complete)
